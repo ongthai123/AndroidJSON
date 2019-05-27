7ebd06c8fc0dd61f29d71f1b5affb22b8eaf097e
Thai <ongthai2803@gmail.com>
2019-05-27 13:18:15 +1200

    JSON completed

---
+++ app/src/main/java/com/example/json/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.json;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view_result);
+        Button button = findViewById(R.id.button_parse);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+
+    }
+
+    private void jsonParse(){
+        String url = "https://api.myjson.com/bins/17ucwk";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+//                        StringBuilder stringBuilder = new StringBuilder();
+
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("students");
+
+                            for(int i =0; i<jsonArray.length(); i++){
+                                JSONObject student = jsonArray.getJSONObject(i);
+
+                                String name = student.getString("name");
+                                int age = student.getInt("age");
+                                String mail = student.getString("mail");
+
+                                textView.append(name + " " + String.valueOf(age) + " " + mail + "\n\n");
+                            }
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(request);
+    }
+}
